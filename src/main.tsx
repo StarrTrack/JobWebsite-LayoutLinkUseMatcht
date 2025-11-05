594206be01fbde28e1179e67afb79fa589db2dc8
PetrStarr <petr.starr@gmail.com>
2025-11-05 16:44:38 +0300

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { MantineProvider, createTheme } from '@mantine/core';
-import { BrowserRouter } from 'react-router-dom';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import {Provider} from 'react-redux';
+import {createTheme, MantineProvider} from '@mantine/core';
+import {BrowserRouter} from 'react-router-dom';
 import '@mantine/core/styles.css';
 import App from './App.tsx';
 import './index.css';
-import { store } from './store/store';
+import {store} from './store/store';
 
 const theme = createTheme({
   colors: {
@@ -32,7 +32,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <MantineProvider theme={theme}>
-        <BrowserRouter>
+        <BrowserRouter basename="/JobWebsite-LayoutLinkUseMatcht">
           <App />
         </BrowserRouter>
       </MantineProvider>
